95ac020124dd2c372af606134e020027dda91a67
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-07-09 18:52:15 +0430

    APi Calls with Axios

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
-  <h1>Events</h1>
+  <div>
+    <h1>Events</h1>
+    <EventCard
+      v-for="(event, index) of events"
+      :key="index"
+      :data-index="index"
+      :event="event"
+    />
+  </div>
 </template>
 
 <script>
 export default {
+  asyncData({ $api, error }) {
+    return $api
+      .get(`/events`)
+      .then((response) => {
+        return {
+          events: response.data,
+        }
+      })
+      .catch(() =>
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time, please try again',
+        })
+      )
+  },
   head: () => ({
     title: 'Event Listing',
   }),
